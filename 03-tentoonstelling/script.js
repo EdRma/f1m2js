205f56a5802334541d2feea5bc9b23d8efda36b4
EdRma <34298@ma-web.nl>
2021-12-21 15:52:21 +0100

    opdracht 3

--- 03-tentoonstelling/script.js
+++ 03-tentoonstelling/script.js
@@ -1,5 +1,25 @@
-console.log("hallo wereld");
+const myImage = document.getElementById("myImage")
+const myTitle = document.getElementById("myTitle");
 
-function changeimage(){
-    console.log("plaatje veranderd")
+let paintings = [
+    "img/painting0.jpg",
+    "img/painting1.jpg",
+    "img/painting2.jpg",
+    "img/painting3.jpg",
+    "img/painting4.jpg",
+    "img/painting5.jpg"
+];
+let title = [
+    "de schilderrijen van van Gogh",
+    "De rode wijngaard",
+    "De sterrennacht",
+    "Sterrennacht boven de Rhône",
+    "Zonnebloemen",
+    "Boerderij in de Provence"
+];
+
+
+function changeimage(index){
+    myTitle.innerHTML = title[index]
+    myImage.src = paintings[index];
 }
