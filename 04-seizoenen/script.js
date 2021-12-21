0c3fb7154018831eb91bbd530fb87c8e4f3cbacf
EdRma <34298@ma-web.nl>
2021-12-21 14:56:25 +0100

    opdracht 4

--- 04-seizoenen/script.js
+++ 04-seizoenen/script.js
@@ -1,19 +1,42 @@
 const myTitle = document.getElementById("myTitle");
 const myImage = document.getElementById("myImage");
 
-let seizoenen =[
-        {
-                   "titel":"lente",
-                   "image":"img/spring.jpg",
-
-        }
+let seizoenen = [
+  {
+        "titel":"seizoenen",
+        "image":"img/seasons.jpg",
+        "backGround":"gray"
+  },
+  {
+        "titel":"lente",
+        "image":"img/spring.jpg",
+        "backGround":"green"
+  },
+  {
+        "titel":"zomer",
+        "image":"img/summer.jpg",
+        "backGround":"yellow"
+  },
+  {
+        "titel":"herfst",
+        "image":"img/autumn.jpg",
+        "backGround":"orange"
+  },
+  {
+        "titel":"winter",
+        "image":"img/winter.jpg",
+        "backGround":"white"
+  }
 ];
+
+
 // let seizoen = { 
 //    "titel":"lente",
 //   "image":"img/spring.jpg",
 // };
 
-function show(){
-        myTitle.innerHTML = seizoen[0].titel;
-        myImage.src = seizoen[0].image;
+function show(index){
+          myTitle.innerHTML = seizoenen[index].titel;
+          myImage.src = seizoenen[index].image;
+          document.body.style.backgroundColor = seizoenen[index].backGround;
 }
