fd0d9a492feb7317f0f389b4f0be37e4f711189a
EdRma <34298@ma-web.nl>
2022-01-25 09:37:51 +0100

    opdracht kat

---
+++ js silvan/opdracht kat/script.js
@@ -0,0 +1,8 @@
+const kat = {
+    naam: "pjoes",
+    leeftijd: 4,
+    hobbies: ["slapen", "miauwen", "drinken"]
+};
+imgElement = document.getElementById("img");
+kat.naam + " is " + kat.leeftijd + " jaar oud.";
+console.log(kat)
